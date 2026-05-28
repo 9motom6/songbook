10f46af4c50d5362528435eca1fdd23fae2fb98f
9motom6 <9motom6@gmail.com>
2026-05-28 21:39:44 +0200

    Better convert to PDF icon

--- src/files-action.ts
+++ src/files-action.ts
@@ -1,3 +1,4 @@
+import { mdiFilePdfBox } from '@mdi/js'
 import axios from '@nextcloud/axios'
 import { showError, showSuccess } from '@nextcloud/dialogs'
 import { emit } from '@nextcloud/event-bus'
@@ -13,7 +14,7 @@ const CHORDPRO_EXTENSIONS = ['.cho', '.crd', '.chopro', '.chordpro', '.pro']
 const action: IFileAction = {
 	id: 'songbook-to-pdf',
 	displayName: () => t('songbook', 'Convert to PDF'),
-	iconSvgInline: () => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z" fill="currentColor"/></svg>',
+	iconSvgInline: () => `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="${mdiFilePdfBox}" fill="currentColor"/></svg>`,
 	enabled: (context: ActionContext) =>
 		context.nodes.length > 0
 		&& context.nodes.every(node =>
